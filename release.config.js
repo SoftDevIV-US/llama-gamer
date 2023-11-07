69a35c5ee8eec7bea581964276e228110940b42c
MorveN11 <morales.patty.jose@gmail.com>
2023-11-07 12:44:41 -0400

    chore: update release config

--- release.config.js
+++ release.config.js
@@ -1,7 +1,18 @@
 const config = {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        releaseRules: [
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "ci", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "docs", release: "patch" },
+        ],
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/github",
     "@semantic-release/npm",
@@ -12,9 +23,16 @@ const config = {
           "package.json",
           "apps/e-commerce-api/dist/**/*.{js,js.map}",
           "apps/e-commerce-client/dist/**/*.{js,js.map}",
+          "CHANGELOG.md",
         ],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
       },
     ],
   ],
