566d94055649ff2f811837aac6a1952c0662b66a
MayerliSantander <mayerlisantander0@gmail.com>
2023-11-22 23:13:55 -0400

    feat: forms to add, edit and info page

--- projects/client/src/app/pages/suppliers/pages/info/SupplierInfo.tsx
+++ projects/client/src/app/pages/suppliers/pages/info/SupplierInfo.tsx
@@ -19,7 +19,7 @@ function SupplierInfo() {
   return (
     <InnerLayout>
       <Form title='Supplier'>
-        <section className='flex h-full flex-col gap-4 overflow-y-scroll px-0 lg:gap-10 lg:px-16 landscape:py-4 landscape:md:py-10'>
+        <section className='flex h-full flex-col gap-4 overflow-y-scroll px-0 lg:gap-6 lg:px-16 landscape:py-4 landscape:md:py-8'>
           {isLoading ? (
             <div className='grid h-full place-content-center py-5'>
               <Loading />
@@ -28,7 +28,8 @@ function SupplierInfo() {
             <>
               <Field title='ID'>{supplier.id}</Field>
               <Field title='Email'>{supplier.email}</Field>
-              <Field title='Delivery Time'>{supplier.deliveryTime}%</Field>
+              <Field title='Delivery Time'>{supplier.deliveryTime} days</Field>
+              <Field title='Country'>{supplier.country.name}</Field>
               <Field title='Created At'>{supplier.createdAt.toString().substring(0, 10)}</Field>
               <Field title='Updated At'>{supplier.updatedAt.toString().substring(0, 10)}</Field>
             </>
