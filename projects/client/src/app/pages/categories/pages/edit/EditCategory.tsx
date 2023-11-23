e1bc0fc4cd84f92fb32d1030f4e1859aef245465
Severaldog <ignaciovillarroel50@gmail.com>
2023-11-23 07:40:44 -0400

    feat: category edit functionality

--- projects/client/src/app/pages/categories/pages/edit/EditCategory.tsx
+++ projects/client/src/app/pages/categories/pages/edit/EditCategory.tsx
@@ -1,16 +1,82 @@
+import EditIcon from '@mui/icons-material/Edit';
+import { Form as FormFormik, Formik } from 'formik';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import InnerLayout from '@/app/layouts/InnerLayout';
+import Button from '@/app/components/button/Button';
+import Form from '@/app/components/form/Form';
+import InputField from '@/app/components/input-field/InputField';
+import Loading from '@/app/components/loading/Loading';
+import NotFound from '@/app/components/not-found/NotFound';
+import { UpdateCategoryDto } from '@/models/category.model';
+
+import useEditCategory from './hooks/useEditCategory';
 
 function EditCategory() {
+  const [isNameCorrect, setIsNameCorrect] = useState<boolean>(true);
+  const [isImageCorrect, setIsImageCorrect] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [found, setFound] = useState<boolean>(true);
   const { id } = useParams<{ id: string }>();
 
+  const { editCategory, category } = useEditCategory({
+    id: String(id),
+    setIsNameCorrect,
+    setIsImageCorrect,
+    setIsLoading,
+    setFound,
+  });
+
   return (
-    <InnerLayout>
-      <div className='grid h-full w-full place-content-center'>
-        <h1 className='text-3xl font-bold'>Edit Category ID: {id}</h1>
+    <Form title='Category'>
+      <div className='h-full overflow-y-scroll'>
+        {isLoading ? (
+          <div className='grid h-full place-content-center py-5'>
+            <Loading />
+          </div>
+        ) : found ? (
+          <Formik
+            initialValues={
+              {
+                name: category.name,
+                image: category.image,
+              } as UpdateCategoryDto
+            }
+            onSubmit={(values) => {
+              editCategory(values);
+            }}
+            enableReinitialize
+          >
+            <FormFormik className='flex h-full flex-col px-0 py-12 lg:px-16 landscape:gap-4 landscape:py-4 landscape:md:gap-20 landscape:md:py-20'>
+              <div className='flex grow flex-col gap-10 font-normal lg:gap-20'>
+                <InputField id='name' value='name' placeholder='Name of the category...' isCorrect={isNameCorrect}>
+                  Name
+                </InputField>
+
+                <InputField
+                  id='image'
+                  value='image'
+                  placeholder='Image for the category...'
+                  type='string'
+                  isCorrect={isImageCorrect}
+                >
+                  <img src={category.image} alt={category.name} />
+                  Image
+                </InputField>
+              </div>
+              <div className='flex justify-center py-6'>
+                <Button className='flex place-items-center gap-2 rounded-xl bg-[#223343] px-6 py-2 text-white' isSubmit>
+                  <EditIcon />
+                  <p className='text-xl'>Edit Category</p>
+                </Button>
+              </div>
+            </FormFormik>
+          </Formik>
+        ) : (
+          <NotFound>Category</NotFound>
+        )}
       </div>
-    </InnerLayout>
+    </Form>
   );
 }
 
--- projects/client/src/app/pages/categories/pages/edit/hooks/useEditCategory.ts
+++ projects/client/src/app/pages/categories/pages/edit/hooks/useEditCategory.ts
@@ -1,7 +1,71 @@
-const useEditCategory = () => {
-  const editCategory = () => {};
+import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
 
-  return { editCategory };
+import useNavigate from '@/app/hooks/useNavigate';
+import { Category, UpdateCategoryDto } from '@/models/category.model';
+import { getCategoryById, updateCategoryById } from '@/services/category.service';
+
+type Props = {
+  id: string;
+  setIsNameCorrect: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsImageCorrect: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  setFound: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const useEditCategory = ({ id, setIsNameCorrect, setIsImageCorrect, setIsLoading, setFound }: Props) => {
+  const [category, setCategory] = useState<Category>({} as Category);
+  const { navigate } = useNavigate();
+
+  useEffect(() => {
+    setIsLoading(true);
+    toast.promise(getCategoryById(id), {
+      loading: 'Loading category...',
+      success: (res) => {
+        setCategory(res);
+        return `Category ${res.name} loaded successfully`;
+      },
+      error: () => {
+        setFound(false);
+        return 'Error Category not found';
+      },
+      finally: () => setIsLoading(false),
+    });
+  }, [id, setIsLoading, setFound]);
+
+  const editCategory = (categoryToUpdate: UpdateCategoryDto) => {
+    const response = toast.promise(updateCategoryById(id, categoryToUpdate), {
+      loading: 'Updating category...',
+      success: (res) => {
+        setIsNameCorrect(true);
+        setIsImageCorrect(true);
+        navigate('/admin/categories');
+        return `Category ${res.name} updated successfully`;
+      },
+      error: (err) => {
+        const { message } = err.response.data;
+        let error = '';
+        if (message instanceof Array) {
+          [error] = message;
+        } else {
+          error = message;
+        }
+        if (error.includes('name')) {
+          setIsNameCorrect(false);
+          setIsImageCorrect(true);
+        }
+        if (error.includes('image')) {
+          setIsImageCorrect(false);
+          setIsNameCorrect(true);
+        }
+        return error;
+      },
+    });
+
+    return response;
+  };
+
+  return { category, editCategory };
 };
 
 export default useEditCategory;
