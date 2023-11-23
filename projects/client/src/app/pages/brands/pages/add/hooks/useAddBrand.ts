98a000e4bbe2c51b3bb1c0c06e7d345155333ccf
Alex Paca <fujorita772@gmail.com>
2023-11-23 00:10:54 -0400

    feat: add forms for brands

--- projects/client/src/app/pages/brands/pages/add/hooks/useAddBrand.ts
+++ projects/client/src/app/pages/brands/pages/add/hooks/useAddBrand.ts
@@ -1,5 +1,47 @@
-const useAddBrand = () => {
-  const addBrand = () => {};
+import { toast } from 'sonner';
+
+import useNavigate from '@/app/hooks/useNavigate';
+import { CreateBrandDto } from '@/models/brand.model';
+import { createBrand } from '@/services/brand.service';
+
+type Props = {
+  setIsNameCorrect: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsLogoCorrect: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const useAddBrand = ({ setIsNameCorrect, setIsLogoCorrect }: Props) => {
+  const { navigate } = useNavigate();
+
+  const addBrand = (brand: CreateBrandDto) => {
+    const response = toast.promise(createBrand(brand), {
+      loading: 'Creating brand...',
+      success: (res) => {
+        setIsNameCorrect(true);
+        setIsLogoCorrect(true);
+        navigate('/admin/brands');
+        return `Brand ${res.name} created successfully`;
+      },
+      error: (err) => {
+        const { message } = err.response.data;
+        let error = '';
+        if (message instanceof Array) {
+          [error] = message;
+        } else {
+          error = message;
+        }
+        if (error.includes('name')) {
+          setIsNameCorrect(false);
+          setIsLogoCorrect(true);
+        }
+        if (error.includes('logo')) {
+          setIsLogoCorrect(false);
+          setIsNameCorrect(true);
+        }
+        return error;
+      },
+    });
+    return response;
+  };
 
   return { addBrand };
 };
--- projects/client/src/app/pages/brands/pages/info/hooks/useLoadBrandInfo.ts
+++ projects/client/src/app/pages/brands/pages/info/hooks/useLoadBrandInfo.ts
@@ -1,7 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { toast } from 'sonner';
 
-const useLoadBrandInfo = () => {
-  useEffect(() => {}, []);
+import { Brand } from '@/models/brand.model';
+import { getBrandById } from '@/services/brand.service';
+
+type Props = {
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsFound: React.Dispatch<React.SetStateAction<boolean>>;
+  id: string;
+};
+
+const useLoadBrandInfo = ({ setIsLoading, setIsFound, id }: Props) => {
+  const [brand, setBrand] = useState<Brand>({} as Brand);
+
+  useEffect(() => {
+    setIsLoading(true);
+    toast.promise(getBrandById(id), {
+      loading: 'Loading brand...',
+      success: (res) => {
+        setBrand(res);
+        setIsFound(true);
+        return `Brand ${res.name} loaded successfully`;
+      },
+      error: () => {
+        setIsFound(false);
+        return 'Error brand not found';
+      },
+      finally: () => setIsLoading(false),
+    });
+  }, [setIsLoading, setIsFound, id]);
+
+  return { brand };
 };
 
 export default useLoadBrandInfo;
