389ca606677f9e3f79b8a462b976f1540819291e
Severaldog <ignaciovillarroel50@gmail.com>
2023-11-23 13:00:37 -0400

    fix: image field style improvement

--- projects/client/src/app/components/image-field/ImageField.tsx
+++ projects/client/src/app/components/image-field/ImageField.tsx
@@ -1,18 +1,19 @@
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 import React, { useState } from 'react';
 
 type Props = {
   id: string;
   children: React.ReactNode;
   value: string;
-  placeholder: string;
   isDisabled?: boolean;
   size?: 'small' | 'medium';
   onFormDataChange: (formData: FormData) => void;
+  isRequired?: boolean;
+  showRequired?: boolean;
 };
 
-function ImageField({ id, children, value, placeholder, isDisabled, size, onFormDataChange }: Props) {
+function ImageField({ id, children, value, isDisabled, size, onFormDataChange, isRequired, showRequired }: Props) {
   const [logoCorrect, setLogoCorrect] = useState<boolean>(true);
-  const [formData, setFormData] = useState<FormData | null>(null);
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,6 +27,7 @@ function ImageField({ id, children, value, placeholder, isDisabled, size, onForm
       if (!isValidExtension) {
         setLogoCorrect(false);
         setPreviewUrl(null);
+        onFormDataChange(new FormData());
         return;
       }
 
@@ -45,20 +47,17 @@ function ImageField({ id, children, value, placeholder, isDisabled, size, onForm
         const isValidSizeSmall = img.width === 256 && img.height === 256;
         const isValidSizeMedium = img.width === 512 && img.height === 512;
 
-        if (size === 'small' && !isValidSizeSmall) {
+        if ((size === 'small' && !isValidSizeSmall) || (size === 'medium' && !isValidSizeMedium)) {
           setLogoCorrect(false);
-        } else if (size === 'medium' && !isValidSizeMedium) {
-          setLogoCorrect(false);
+          setPreviewUrl(null);
+          onFormDataChange(new FormData());
         } else {
           setLogoCorrect(true);
           setPreviewUrl(result);
           const data = new FormData();
           data.append('file', file);
-          setFormData(data);
           onFormDataChange(data);
-          return formData;
         }
-        return null;
       };
 
       reader.readAsDataURL(file);
@@ -69,26 +68,41 @@ function ImageField({ id, children, value, placeholder, isDisabled, size, onForm
   };
 
   return (
-    <label htmlFor={id} className='block text-gray-700'>
+    <label htmlFor={id} className='block rounded-lg text-gray-700'>
       <p className='px-4 text-[#1B263B]/50'>
         {children}
-        <span className='text-red-700'> *</span>
+        {showRequired && <span className='text-red-700'> *</span>}
       </p>
-      <input
-        id={id}
-        name={value}
-        type='file'
-        accept='image/png'
-        className={`w-full rounded-lg border px-3 py-4 ${
-          logoCorrect ? 'border-gray-300 focus:border-blue-500 focus:outline-none' : 'border-red-500'
-        }`}
-        placeholder={placeholder}
-        onChange={handleFileChange}
-        required
-        disabled={isDisabled}
-      />
-      {previewUrl && <img src={previewUrl} alt='Preview' className='mb-2 max-h-40 rounded-lg' />}
-      {!logoCorrect && <p className='mt-2 text-sm text-red-500'>Select a valid PNG image.</p>}
+      <div className=' border-2 border-dashed bg-slate-50'>
+        <input
+          id={id}
+          name={value}
+          type='file'
+          accept='image/png'
+          className='left-0 top-0 h-full w-full cursor-pointer opacity-0'
+          onChange={handleFileChange}
+          required={isRequired}
+          disabled={isDisabled}
+        />
+        <div
+          className={`w-full rounded-lg px-3 py-4 ${
+            logoCorrect ? 'border-gray-300 focus:border-blue-500 focus:outline-none' : 'border-red-500'
+          }`}
+        >
+          {previewUrl && <img src={previewUrl} alt='Preview' className='mb-2 max-h-40 rounded-lg' />}
+          {!previewUrl && (
+            <div className='flex flex-col items-center'>
+              <div className='mb-10'>
+                <span className='text-gray-500'>Drag the image here to upload...</span>
+              </div>
+              <div>
+                <ImageOutlinedIcon className='mb-10 scale-[3] text-gray-300' />
+              </div>
+            </div>
+          )}
+          {!logoCorrect && <p className='mt-2 text-sm text-red-500'>Select a valid PNG image.</p>}
+        </div>
+      </div>
     </label>
   );
 }
@@ -96,6 +110,8 @@ function ImageField({ id, children, value, placeholder, isDisabled, size, onForm
 ImageField.defaultProps = {
   isDisabled: false,
   size: 'small',
+  isRequired: true,
+  showRequired: true,
 };
 
 export default ImageField;
