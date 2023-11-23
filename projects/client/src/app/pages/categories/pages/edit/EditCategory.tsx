1df53a0ff34c10ec9fd435c30ded8b6bf98cb248
Severaldog <ignaciovillarroel50@gmail.com>
2023-11-23 13:05:18 -0400

    fix: edit category refactor and field required sign optional

--- projects/client/src/app/pages/categories/pages/edit/EditCategory.tsx
+++ projects/client/src/app/pages/categories/pages/edit/EditCategory.tsx
@@ -5,16 +5,19 @@ import { useParams } from 'react-router-dom';
 
 import Button from '@/app/components/button/Button';
 import Form from '@/app/components/form/Form';
+import ImageField from '@/app/components/image-field/ImageField';
 import InputField from '@/app/components/input-field/InputField';
 import Loading from '@/app/components/loading/Loading';
 import NotFound from '@/app/components/not-found/NotFound';
 import { UpdateCategoryDto } from '@/models/category.model';
+import uploadImage from '@/services/cloudinary.service';
 
 import useEditCategory from './hooks/useEditCategory';
 
 function EditCategory() {
   const [isNameCorrect, setIsNameCorrect] = useState<boolean>(true);
-  const [isImageCorrect, setIsImageCorrect] = useState<boolean>(true);
+  const [, setIsImageCorrect] = useState<boolean>(true);
+  const [imageFormData, setImageFormData] = useState<FormData | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [found, setFound] = useState<boolean>(true);
   const { id } = useParams<{ id: string }>();
@@ -42,27 +45,51 @@ function EditCategory() {
                 image: category.image,
               } as UpdateCategoryDto
             }
-            onSubmit={(values) => {
-              editCategory(values);
+            onSubmit={async (values) => {
+              if (imageFormData) {
+                try {
+                  const response = await uploadImage(imageFormData);
+
+                  const imageUrl: string = response.data.secure_url;
+
+                  const createCategory = {
+                    name: values.name,
+                    image: imageUrl,
+                  } as UpdateCategoryDto;
+
+                  editCategory(createCategory);
+
+                  setImageFormData(null);
+                } catch (error: any) {
+                  throw new Error(error);
+                }
+              }
             }}
             enableReinitialize
           >
             <FormFormik className='flex h-full flex-col px-0 py-12 lg:px-16 landscape:gap-4 landscape:py-4 landscape:md:gap-20 landscape:md:py-20'>
               <div className='flex grow flex-col gap-10 font-normal lg:gap-20'>
-                <InputField id='name' value='name' placeholder='Name of the category...' isCorrect={isNameCorrect}>
+                <InputField
+                  id='name'
+                  value='name'
+                  placeholder='Name of the category...'
+                  isCorrect={isNameCorrect}
+                  showRequired={false}
+                >
                   Name
                 </InputField>
 
-                <InputField
-                  id='image'
-                  value='image'
-                  placeholder='Image for the category...'
-                  type='string'
-                  isCorrect={isImageCorrect}
+                <ImageField
+                  id='logo'
+                  value='logo'
+                  size='medium'
+                  onFormDataChange={(formData) => setImageFormData(formData)}
+                  isRequired={false}
+                  showRequired={false}
                 >
-                  <img src={category.image} alt={category.name} />
                   Image
-                </InputField>
+                </ImageField>
+                <img src={category.image} alt={category.name} style={{ width: '300px', height: 'auto' }} />
               </div>
               <div className='flex justify-center py-6'>
                 <Button className='flex place-items-center gap-2 rounded-xl bg-[#223343] px-6 py-2 text-white' isSubmit>
