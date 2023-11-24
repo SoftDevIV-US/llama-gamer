63e9d73df1ae2033e55a5eebc710cef00789bd4a
Battensalpozz <sebasthiansalinaspozzo@gmail.com>
2023-11-23 23:08:32 -0400

    fix: missed about and acount with the comments

--- projects/client/src/app/pages/account/account.tsx
+++ projects/client/src/app/pages/account/account.tsx
@@ -1,11 +1,11 @@
 import InnerLayout from '@/app/layouts/InnerLayout';
 
 function Account() {
-  const aboutPageText = 'About Page';
+  const accountPageText = 'Account Page';
   return (
     <InnerLayout>
       <div className='grid h-full w-full place-content-center '>
-        <h1 className='text-3xl font-bold'>{aboutPageText} </h1>
+        <h1 className='text-3xl font-bold'>{accountPageText} </h1>
       </div>
     </InnerLayout>
   );
