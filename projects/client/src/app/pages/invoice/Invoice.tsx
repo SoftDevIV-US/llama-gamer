b8242e1ff6b46075cffaa8dd1b64f7709b1f393d
Alex Paca <fujorita772@gmail.com>
2023-12-02 18:21:19 -0400

    fix: invoice flashes

--- projects/client/src/app/pages/invoice/Invoice.tsx
+++ projects/client/src/app/pages/invoice/Invoice.tsx
@@ -1,4 +1,4 @@
-import { Document, Image, Page, PDFViewer, Text, View } from '@react-pdf/renderer';
+import { Document, Image, Page, Text, View } from '@react-pdf/renderer';
 
 import { CartProduct } from '@/models/product.model';
 import useAuthStore from '@/store/auth.store';
@@ -15,98 +15,97 @@ function InvoicePDF({ cart, totalPrice }: Props) {
   const formattedDate = currentDate.toLocaleDateString('es-ES');
   const userInfo = useAuthStore.getState().auth?.user;
 
-  const tax = 0.13;
+  const tax = (0.13).toFixed(2);
 
   const subtotal = totalPrice.toFixed(2);
-  const totalTax = (Number((tax + totalPrice).toFixed(2)) - Number(totalPrice.toFixed(2))).toFixed(2);
-  const total = (tax + totalPrice).toFixed(2);
+  const totalTax = (parseFloat(tax) + totalPrice).toFixed(2);
+  const total = (parseFloat(tax) + totalPrice).toFixed(2);
 
   return (
-    <PDFViewer>
-      <Document>
-        <Page
-          size='A4'
+    <Document>
+      <Page
+        size='A4'
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          backgroundColor: 'white',
+        }}
+      >
+        <View
           style={{
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'flex-start',
             alignItems: 'center',
             backgroundColor: 'white',
+            padding: 10,
           }}
         >
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'flex-start',
-              alignItems: 'center',
-              backgroundColor: 'white',
-              padding: 10,
-            }}
-          >
-            <View style={{ marginBottom: 12, flexDirection: 'row', alignItems: 'center' }}>
-              <Image src={logo} style={{ maxWidth: '300px', maxHeight: '300px', marginRight: 120 }} />
-              <Text style={{ fontSize: '24px', fontWeight: 'bold', color: '#3A4D5E', marginLeft: -50 }}>Billing</Text>
+          <View style={{ marginBottom: 12, flexDirection: 'row', alignItems: 'center' }}>
+            <Image src={logo} style={{ maxWidth: '300px', maxHeight: '300px', marginRight: 120 }} />
+            <Text style={{ fontSize: '24px', fontWeight: 'bold', color: '#3A4D5E', marginLeft: -50 }}>Billing</Text>
+          </View>
+
+          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+            <View style={{ textAlign: 'center', marginRight: 120 }}>
+              <Text style={{ fontSize: '16px', color: '#111111' }}>{`${userInfo?.name} ${userInfo?.lastName}`}</Text>
+              <Text style={{ fontSize: '16px', color: '#111111' }}>{userInfo?.email}</Text>
             </View>
 
-            <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-              <View style={{ textAlign: 'center', marginRight: 120 }}>
-                <Text style={{ fontSize: '16px', color: '#111111' }}>{`${userInfo?.name} ${userInfo?.lastName}`}</Text>
-                <Text style={{ fontSize: '16px', color: '#111111' }}>{userInfo?.email}</Text>
-              </View>
-
-              <View style={{ textAlign: 'center', marginLeft: 120 }}>
-                <Text style={{ fontSize: '16px', fontWeight: 'bold', color: '#111111', marginLeft: 20 }}>Date</Text>
-                <Text style={{ fontSize: '16px', color: '#111111' }}>{formattedDate}</Text>
-              </View>
-            </View>
-
-            <View style={{ marginTop: 12 }}>
-              <View style={{ flexDirection: 'row' }}>
-                <Text style={{ width: '32%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
-                  Name
-                </Text>
-                <Text style={{ width: '13%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
-                  Amount
-                </Text>
-                <Text style={{ width: '15%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
-                  Unitary P.
-                </Text>
-                <Text style={{ width: '20%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
-                  Tax
-                </Text>
-                <Text style={{ width: '20%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
-                  Total
-                </Text>
-              </View>
-              {cart.map((item) => (
-                <View key={item.product.id} style={{ flexDirection: 'row' }}>
-                  <Text style={{ width: '32%' }}>{item.product.name}</Text>
-                  <Text style={{ width: '13%', textAlign: 'center' }}>{item.quantity}</Text>
-                  <Text style={{ width: '15%', textAlign: 'center' }}>{item.product.price}</Text>
-                  <Text style={{ width: '20%', textAlign: 'center' }}>{`${
-                    item.totalPrice - item.priceWithoutTax
-                  } Bs.`}</Text>
-                  <Text style={{ width: '20%', textAlign: 'center' }}>{`${item.totalPrice} Bs.`}</Text>
-                </View>
-              ))}
-              <View style={{ flexDirection: 'row', marginTop: 10 }}>
-                <Text style={{ width: '80%', fontWeight: 'bold', textAlign: 'right' }}>Subtotal</Text>
-                <Text style={{ width: '20%', textAlign: 'right' }}>{` ${subtotal} Bs.`}</Text>
-              </View>
-              <View style={{ flexDirection: 'row' }}>
-                <Text style={{ width: '80%', fontWeight: 'bold', textAlign: 'right' }}>Taxes</Text>
-                <Text style={{ width: '20%', textAlign: 'right' }}>{`${totalTax} Bs.`}</Text>
-              </View>
-              <View style={{ flexDirection: 'row' }}>
-                <Text style={{ width: '80%', fontWeight: 'bold', textAlign: 'right' }}>Total</Text>
-                <Text style={{ width: '20%', textAlign: 'right' }}>{`${total} Bs.`}</Text>
-              </View>
+            <View style={{ textAlign: 'center', marginLeft: 120 }}>
+              <Text style={{ fontSize: '16px', fontWeight: 'bold', color: '#111111', marginLeft: 20 }}>Date</Text>
+              <Text style={{ fontSize: '16px', color: '#111111' }}>{formattedDate}</Text>
             </View>
           </View>
-        </Page>
-      </Document>
-    </PDFViewer>
+
+          <View style={{ marginTop: 12 }}>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={{ width: '32%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
+                Name
+              </Text>
+              <Text style={{ width: '13%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
+                Amount
+              </Text>
+              <Text style={{ width: '15%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
+                Unitary P.
+              </Text>
+              <Text style={{ width: '20%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
+                Tax
+              </Text>
+              <Text style={{ width: '20%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
+                Total
+              </Text>
+            </View>
+            {cart.map((item) => (
+              <View key={item.product.id} style={{ flexDirection: 'row' }}>
+                <Text style={{ width: '32%' }}>{item.product.name}</Text>
+                <Text style={{ width: '13%', textAlign: 'center' }}>{item.quantity}</Text>
+                <Text style={{ width: '15%', textAlign: 'center' }}>{item.product.price}</Text>
+                <Text style={{ width: '20%', textAlign: 'center' }}>{`${(
+                  item.totalPrice - item.priceWithoutTax
+                ).toFixed(2)} Bs.`}</Text>
+
+                <Text style={{ width: '20%', textAlign: 'center' }}>{`${item.totalPrice.toFixed(2)} Bs.`}</Text>
+              </View>
+            ))}
+            <View style={{ flexDirection: 'row', marginTop: 10 }}>
+              <Text style={{ width: '80%', fontWeight: 'bold', textAlign: 'right' }}>Subtotal</Text>
+              <Text style={{ width: '20%', textAlign: 'right' }}>{` ${subtotal} Bs.`}</Text>
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={{ width: '80%', fontWeight: 'bold', textAlign: 'right' }}>Taxes</Text>
+              <Text style={{ width: '20%', textAlign: 'right' }}>{`${totalTax} Bs.`}</Text>
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={{ width: '80%', fontWeight: 'bold', textAlign: 'right' }}>Total</Text>
+              <Text style={{ width: '20%', textAlign: 'right' }}>{`${total} Bs.`}</Text>
+            </View>
+          </View>
+        </View>
+      </Page>
+    </Document>
   );
 }
 
--- projects/client/src/app/pages/invoice/InvoiceModal.tsx
+++ projects/client/src/app/pages/invoice/InvoiceModal.tsx
@@ -38,22 +38,20 @@ function InvoiceModal({ id, name, open, onClose, cart, totalPrice }: Props) {
         className='flex items-center justify-center'
       >
         <Box className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-black bg-white px-4 pb-3 pt-2 shadow-md'>
-          <div className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-gray-300 bg-white'>
-            <PDFViewer style={{ width: '500px', height: '600px' }}>
-              <DocuPDF cart={cart} totalPrice={totalPrice} />
-            </PDFViewer>
-            <button
-              type='button'
-              className='mt-4 bg-blue-500 px-4 py-2 text-white hover:bg-blue-700 focus:border-blue-300 focus:outline-none focus:ring'
-              onClick={handleClose}
-            >
-              Close Modal
-            </button>
-            <div>
-              <PDFDownloadLink document={<DocuPDF cart={cart} totalPrice={totalPrice} />} fileName={`${name}.pdf`}>
-                <Button type='button'> Download PDF</Button>
-              </PDFDownloadLink>
-            </div>
+          <PDFViewer style={{ width: '500px', height: '600px' }}>
+            <DocuPDF cart={cart} totalPrice={totalPrice} />
+          </PDFViewer>
+          <button
+            type='button'
+            className='mt-4 bg-blue-500 px-4 py-2 text-white hover:bg-blue-700 focus:border-blue-300 focus:outline-none focus:ring'
+            onClick={handleClose}
+          >
+            Close Modal
+          </button>
+          <div>
+            <PDFDownloadLink document={<DocuPDF cart={cart} totalPrice={totalPrice} />} fileName={`${name}.pdf`}>
+              <Button type='button'> Download PDF</Button>
+            </PDFDownloadLink>
           </div>
         </Box>
       </Modal>
