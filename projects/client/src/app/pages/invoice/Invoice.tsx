f9540b5a18fb0eb11f7e87d1ac5c0b6342b58b25
MorveN11 <morales.patty.jose@gmail.com>
2023-12-04 18:55:58 -0400

    fix: issue 122 124

--- projects/client/src/app/pages/invoice/Invoice.tsx
+++ projects/client/src/app/pages/invoice/Invoice.tsx
@@ -1,4 +1,5 @@
-import { Document, Image, Page, Text, View } from '@react-pdf/renderer';
+import { Document, Image, Line, Page, Svg, Text, View } from '@react-pdf/renderer';
+import React from 'react';
 
 import { CartProduct } from '@/models/product.model';
 import useAuthStore from '@/store/auth.store';
@@ -22,85 +23,184 @@ function InvoicePDF({ cart, totalPrice }: Props) {
   const totalTax = (Number(total) - Number(subtotal)).toFixed(2);
 
   return (
-    <Document>
-      <Page
-        size='A4'
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-          backgroundColor: 'white',
-        }}
-      >
+    <Document title='Invoice'>
+      <Page size='A4'>
         <View
           style={{
             display: 'flex',
             flexDirection: 'column',
-            justifyContent: 'flex-start',
-            alignItems: 'center',
             backgroundColor: 'white',
-            padding: 10,
+            paddingHorizontal: 50,
+            paddingVertical: 15,
+            gap: 5,
           }}
         >
-          <View style={{ marginBottom: 12, flexDirection: 'row', alignItems: 'center' }}>
-            <Image src={logo} style={{ maxWidth: '300px', maxHeight: '300px', marginRight: 120 }} />
-            <Text style={{ fontSize: '24px', fontWeight: 'bold', color: '#3A4D5E', marginLeft: -50 }}>Billing</Text>
+          <View
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              width: '100%',
+              flexDirection: 'row',
+              alignContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Image src={logo} style={{ maxWidth: '300px', maxHeight: '300px', marginLeft: -30 }} />
+            <Text
+              style={{
+                fontSize: '24px',
+                fontFamily: 'Helvetica-Bold',
+                color: '#3A4D5E',
+                marginRight: 50,
+              }}
+            >
+              Billing
+            </Text>
           </View>
 
-          <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-            <View style={{ textAlign: 'center', marginRight: 120 }}>
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              width: '100%',
+              marginBottom: 40,
+            }}
+          >
+            <View style={{ textAlign: 'left', marginLeft: 25, flexDirection: 'column', gap: 5 }}>
               <Text style={{ fontSize: '16px', color: '#111111' }}>{`${userInfo?.name} ${userInfo?.lastName}`}</Text>
               <Text style={{ fontSize: '16px', color: '#111111' }}>{userInfo?.email}</Text>
             </View>
 
-            <View style={{ textAlign: 'center', marginLeft: 120 }}>
-              <Text style={{ fontSize: '16px', fontWeight: 'bold', color: '#111111', marginLeft: 20 }}>Date</Text>
+            <View style={{ marginRight: 50, flexDirection: 'column', gap: 5 }}>
+              <Text style={{ fontSize: '16px', fontFamily: 'Helvetica-Bold', color: '#111111', marginLeft: 20 }}>
+                Date
+              </Text>
               <Text style={{ fontSize: '16px', color: '#111111' }}>{formattedDate}</Text>
             </View>
           </View>
 
-          <View style={{ marginTop: 12 }}>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={{ width: '32%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
-                Name
-              </Text>
-              <Text style={{ width: '13%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
-                Amount
-              </Text>
-              <Text style={{ width: '15%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
-                Unitary P.
-              </Text>
-              <Text style={{ width: '20%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
-                Tax
-              </Text>
-              <Text style={{ width: '20%', fontWeight: 'bold', backgroundColor: '#3A4D5E', color: '#FFFFFF' }}>
-                Total
-              </Text>
-            </View>
-            {cart.map((item) => (
-              <View key={item.product.id} style={{ flexDirection: 'row' }}>
-                <Text style={{ width: '32%' }}>{item.product.name}</Text>
-                <Text style={{ width: '13%', textAlign: 'center' }}>{item.quantity}</Text>
-                <Text style={{ width: '15%', textAlign: 'center' }}>{item.product.price}</Text>
-                <Text style={{ width: '20%', textAlign: 'center' }}>{`${(item.totalPrice * item.tax).toFixed(
-                  2
-                )} Bs.`}</Text>
-
-                <Text style={{ width: '20%', textAlign: 'center' }}>{`${item.totalPrice.toFixed(2)} Bs.`}</Text>
+          <View>
+            <View
+              style={{
+                flexDirection: 'row',
+                display: 'flex',
+                width: '100%',
+                height: '40px',
+                backgroundColor: '#3A4D5E',
+                fontSize: '14px',
+                gap: 5,
+                fontFamily: 'Helvetica-Bold',
+                color: '#FFFFFF',
+              }}
+            >
+              <View
+                style={{
+                  width: '30%',
+                  paddingLeft: 25,
+                  justifyContent: 'center',
+                }}
+              >
+                <Text>Name</Text>
               </View>
+              <View
+                style={{
+                  width: '10%',
+                  justifyContent: 'center',
+                  textAlign: 'center',
+                }}
+              >
+                <Text>Amount</Text>
+              </View>
+              <View
+                style={{
+                  width: '20%',
+                  justifyContent: 'center',
+                  textAlign: 'right',
+                }}
+              >
+                <Text>Unitary P.</Text>
+              </View>
+              <View
+                style={{
+                  width: '20%',
+                  justifyContent: 'center',
+                  textAlign: 'right',
+                }}
+              >
+                <Text>Supplier Tax</Text>
+              </View>
+              <View
+                style={{
+                  width: '20%',
+                  justifyContent: 'center',
+                  textAlign: 'right',
+                  paddingRight: 30,
+                }}
+              >
+                <Text>Total</Text>
+              </View>
+            </View>
+            {cart.map((item, index) => (
+              <React.Fragment key={item.product.id}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    marginTop: 15,
+                    fontSize: '14px',
+                    gap: 5,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignContent: 'center',
+                  }}
+                >
+                  <Text style={{ width: '30%', paddingLeft: 25 }}>{item.product.name}</Text>
+                  <Text style={{ width: '10%', textAlign: 'center' }}>{item.quantity}</Text>
+                  <Text style={{ width: '20%', textAlign: 'right' }}>{item.product.price} Bs</Text>
+                  <Text style={{ width: '20%', textAlign: 'right', paddingRight: 10 }}>{`${(
+                    item.totalPrice * item.tax
+                  ).toFixed(2)} Bs`}</Text>
+
+                  <Text style={{ width: '20%', textAlign: 'right', paddingRight: 15 }}>{`${item.totalPrice.toFixed(
+                    2
+                  )} Bs`}</Text>
+                </View>
+                {index !== cart.length - 1 && (
+                  <Svg
+                    height='5'
+                    width='100%'
+                    style={{
+                      marginTop: 15,
+                    }}
+                  >
+                    <Line x1='0' y1='5' x2='500' y2='5' strokeWidth={2} stroke='rgb(196,196,196)' />
+                  </Svg>
+                )}
+              </React.Fragment>
             ))}
-            <View style={{ flexDirection: 'row', marginTop: 10 }}>
-              <Text style={{ width: '80%', fontWeight: 'bold', textAlign: 'right' }}>Subtotal</Text>
-              <Text style={{ width: '20%', textAlign: 'right' }}>{` ${subtotal} Bs.`}</Text>
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={{ width: '80%', fontWeight: 'bold', textAlign: 'right' }}>Taxes</Text>
-              <Text style={{ width: '20%', textAlign: 'right' }}>{`${totalTax} Bs.`}</Text>
-            </View>
-            <View style={{ flexDirection: 'row' }}>
-              <Text style={{ width: '80%', fontWeight: 'bold', textAlign: 'right' }}>Total</Text>
-              <Text style={{ width: '20%', textAlign: 'right' }}>{`${total} Bs.`}</Text>
+            <View
+              style={{
+                flexDirection: 'column',
+                gap: 30,
+                marginTop: 50,
+                fontSize: '14px',
+                textAlign: 'right',
+                marginRight: 15,
+              }}
+            >
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={{ width: '80%', fontFamily: 'Helvetica-Bold' }}>Subtotal</Text>
+                <Text style={{ width: '20%' }}>{` ${subtotal} Bs`}</Text>
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={{ width: '80%', fontFamily: 'Helvetica-Bold' }}>Region Tax</Text>
+                <Text style={{ width: '20%' }}>{`${totalTax} Bs`}</Text>
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <Text style={{ width: '80%', fontFamily: 'Helvetica-Bold' }}>Total</Text>
+                <Text style={{ width: '20%', fontFamily: 'Helvetica-Bold' }}>{`${total} Bs`}</Text>
+              </View>
             </View>
           </View>
         </View>
